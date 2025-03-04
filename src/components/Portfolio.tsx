64716c5ea5e62c510a6e0710750a2eb432b589f0
justnoted <just.noted3505@gmail.com>
2025-03-04 15:36:42 -0600

    Added portfolio layout sections

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,6 +1,15 @@
+import Skills from "@/components/portfolio/Skills";
+import Experience from "@/components/portfolio/Experience";
+import Education from "@/components/portfolio/Education";
+import Projects from "@/components/portfolio/Projects";
+
 export default function Portfolio() {
   return (
-    <div>
-    </div>
+    <main>
+      <Skills />
+      <Experience />
+      <Education />
+      <Projects />
+    </main>
   )
 }
