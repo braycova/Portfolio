172b1091f6ede56ffd46d877fabbb57108d164ba
justnoted <just.noted3505@gmail.com>
2025-03-04 18:03:13 -0600

    Fixed Skills image align

--- src/components/portfolio/Skills.tsx
+++ src/components/portfolio/Skills.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Skills() {
   const Item = ({imageUrl, name, description}: {imageUrl: string, name: string, description: string}) => {
     return (
-      <div className="bg-gray-800 border-1 border-gray-700 rounded-lg flex p-3">
-        <Image src={imageUrl} alt={name} width={50} height={50} className="rounded-lg object-contain" />
+      <div className="bg-gray-800 border-1 border-gray-700 rounded-lg flex p-3 items-center">
+        <Image src={imageUrl} alt={name} width={50} height={50} className="rounded-lg object-contain h-15 w-15" />
         <div className="flex flex-col p-2 justify-center">
           <span className="font-bold text-lg">{name}</span>
           <span className="opacity-80 text-sm">{description}</span>
