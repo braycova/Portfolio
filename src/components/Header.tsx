0a6af31d70fe501689f706af1be85c0fec44e167
justnoted <just.noted3505@gmail.com>
2025-03-05 02:18:16 -0600

    Updated Header content, styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,14 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot, faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Intro from "@/components/header/Intro";
+import Navigation from "@/components/header/Navigation";
+import Contact from "@/components/header/Contact";
 
 export default function Header() {
-  const links = ["Skills", "Experience", "Education", "Projects"]
-
   return (
-    <div className="w-1/2 lg:h-screen lg:sticky lg:top-0">
-      <header className="mt-7 p-7 border-2 rounded-lg">
-        <div>
-          <h1 className="font-bold text-4xl"><a href="#">Brayan Covarrubias</a></h1>
-          <p className="text-2xl">Software Developer</p>
-          <p className="mt-1 text-lg"><FontAwesomeIcon icon={faLocationDot} className="fa-fw"/> Iowa, USA</p>
-          <p className="mt-2 opacity-80">An up-and-coming software developer, on route to Des Moines as an intern at
-            Principal Financial this May.</p>
-        </div>
-        <nav className="grid grid-cols-2 gap-2">
-          {links.map((link) => (
-            <a key={link} href={`#${link.toLowerCase()}`} className="text-lg border-gray-900">
-              {link}
-            </a>
-          ))}
-        </nav>
-        <nav className="pt-2 text-3xl">
-          <a href="mailto:brayan.cova@outlook.com" target="_blank" title="Shoot me an email!"><FontAwesomeIcon
-            icon={faEnvelope} className="fa-fw"/></a>
-          <a href="https://github.com/justnoted" target="_blank" title="GitHub"><FontAwesomeIcon icon={faGithub}
-                                                                                                 className="fa-fw"/></a>
-          <a href="https://www.linkedin.com/in/brayan-covarrubias-segura/" target="_blank"
-             title="LinkedIn"><FontAwesomeIcon icon={faLinkedin} className="fa-fw"/></a>
-        </nav>
+    <div className="lg:w-1/2 lg:h-screen lg:sticky lg:top-0">
+      <header className="mt-7 p-7 lg:border-2 lg:rounded-lg border-b-2 border-gray-700">
+        <Intro />
+        <Navigation />
+        <Contact />
       </header>
     </div>
   )
