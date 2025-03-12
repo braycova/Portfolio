614486037eec9d2daf917942ab23a8c1f37db1d9
justnoted <just.noted3505@gmail.com>
2025-03-12 14:25:43 -0500

    Updated Skills

--- src/components/portfolio/SectionTitle.tsx
+++ src/components/portfolio/SectionTitle.tsx
@@ -1,7 +1,7 @@
 export default function SectionTitle({title}: {title: string}) {
   return (
     <div className="flex items-center gap-3">
-      <h1 className="font-bold text-xl lg:text-3xl">{title}</h1>
+      <h1 className="font-bold text-xl md:text-2xl">{title}</h1>
       <span className="border-b-2 dark:border-amber-50 flex-grow"></span>
     </div>
   )
