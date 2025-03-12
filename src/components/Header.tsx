5c6e01486f00cd28a511af54c875c794cb524397
justnoted <just.noted3505@gmail.com>
2025-03-11 23:11:01 -0500

    Updated Header, added data file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
-import Intro from "@/components/header/Intro";
-import Navigation from "@/components/header/Navigation";
-import Contact from "@/components/header/Contact";
+import Image from "next/image";
+import { location, description, contacts } from "@/data/header";
+import { Tooltip } from '@mantine/core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-    <div className="lg:w-1/2 lg:h-screen lg:sticky lg:top-0">
-      <header className="mt-7 p-7 lg:border-2 lg:rounded-lg border-b-2 border-gray-700">
-        <Intro />
-        <Navigation />
-        <Contact />
-      </header>
-    </div>
+    <header className="p-7 border-b-2 border-gray-700">
+      <Image src="/Profile.jpg" alt="Profile image" width={200} height={200} className="rounded-4xl mb-1 border-2 border-gray-800 hover:border-indigo-700 transition"/>
+      <h1 className="font-bold text-4xl">Brayan Covarrubias</h1>
+      <p className="text-2xl">Software Developer</p>
+      <p className="mt-1 text-lg"><FontAwesomeIcon icon={faLocationDot} className="fa-fw"/> {location}</p>
+      <p className="mt-2 text-gray-300">{description}</p>
+
+      <nav className="mt-2 text-3xl" aria-label="Email and Social Links">
+        {contacts.map((contact) => (
+          <Tooltip key={contact.name} label={contact.label} arrowSize={4} withArrow color="indigo">
+            <a href={contact.link} target="_blank" className="hover:text-blue-500 transition" aria-label={`${contact.name} (opens as new tab)`} >
+              <FontAwesomeIcon icon={contact.icon} className="fa-fw"/>
+            </a>
+          </Tooltip>
+        ))}
+      </nav>
+    </header>
   )
 }
